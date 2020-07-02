a13ee44a96cbff317bde2fc8de34a00597e4e8d4
Jesse Cape <jesse.cape@cfacorp.com>
2020-07-01 22:05:33 -0400

    Style and layout refinement

--- app/components/CategoriesList.js
+++ app/components/CategoriesList.js
@@ -1,30 +1,74 @@
 import React from 'react';
-import {FlatList, View, Text} from 'react-native';
+import {FlatList, View, StyleSheet} from 'react-native';
 import Category from './Category';
 
 const DATA = [
   {
     header: 'All Coupons',
     description: 'Find local deals in all categories!',
+    iconName: 'star',
     id: '1',
   },
   {
     header: 'Auto',
     description: 'Oil Change, Car Wash, Tires, Repair...',
+    iconName: 'car',
+    id: '2',
+  },
+  {
+    header: 'Restaurants',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'silverware',
+    id: '1',
+  },
+  {
+    header: 'Home Improvement',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'hammer',
+    id: '2',
+  },
+  {
+    header: 'Home Services',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'home',
+    id: '1',
+  },
+  {
+    header: 'Retail',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'shopping',
+    id: '2',
+  },
+  {
+    header: 'Garden & Landscape',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'leaf',
+    id: '1',
+  },
+  {
+    header: 'Personal Services',
+    description: 'blah blah blah blah blah blah',
+    iconName: 'account',
     id: '2',
   },
 ];
 
+const styles = StyleSheet.create({
+  listWrapper: {backgroundColor: '#ddd', flex: 1},
+});
+
 const CategoriesList = () => (
-  <View>
-    <Text>Testing</Text>
+  <View style={styles.listWrapper}>
     <FlatList
       data={DATA}
       renderItem={({item}) => (
-        <Category header={item.header} text={item.description} />
+        <Category
+          header={item.header}
+          text={item.description}
+          icon={item.iconName}
+        />
       )}
     />
-    <Text>Testing</Text>
   </View>
 );
 
--- app/components/Category.js
+++ app/components/Category.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
 
 const Category = ({icon, header, text}) => (
-  <View style={{flex: 1, flexDirection: 'row'}}>
-    <Text style={{flex: 1}}>{header}</Text>
-    <Text style={{flex: 1}}>{text}</Text>
+  <View style={styles.body}>
+    <View style={styles.iconWrapper}>
+      <Icon style={styles.icon} name={icon} />
+    </View>
+    <View style={styles.contentWrapper}>
+      <Text style={styles.header}>{header}</Text>
+      <Text style={styles.description}>{text}</Text>
+    </View>
   </View>
 );
 
+const styles = StyleSheet.create({
+  body: {flex: 1, flexDirection: 'row', margin: 5, backgroundColor: '#f1f1f1'},
+  iconWrapper: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  icon: {fontSize: 25},
+  contentWrapper: {flex: 6, flexDirection: 'column'},
+  header: {flex: 1, fontWeight: 'bold', fontFamily: 'sans-serif-light'},
+  description: {flex: 1, fontSize: 12, fontFamily: 'sans-serif-light'},
+});
+
 Category.propTypes = {
+  icon: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   header: PropTypes.string.isRequired,
 };
