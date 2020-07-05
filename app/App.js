3e1cb7bcdf2125792009ee9ef4958931deddc08f
Jesse Cape <jesse.cape@cfacorp.com>
2020-07-05 15:45:39 -0400

    Added babel-polyfill dependency for cross-fetch

--- app/App.js
+++ app/App.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import 'babel-polyfill'; // [todo]: determine if needed for 'cross-fetch'
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,3 +1,5 @@
+import fetch from 'cross-fetch';
+
 export const REQUEST_CATEGORIES = 'REQUEST_CATEGORIES';
 export const RECEIVE_CATEGORIES = 'RECEIVE_POSTS';
 
@@ -12,5 +14,38 @@ function receiveCategories(json) {
     type: RECEIVE_CATEGORIES,
     posts: json.data.children.map((child) => child.data),
     receivedAt: Date.now(),
+  };
+}
+
+export function fetchPosts() {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return function (dispatch) {
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(requestCategories());
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch('https://swapi.dev/api/people/1/')
+      .then(
+        (response) => response.json(),
+        // Do not use catch, because errors occured during rendering
+        // should be handled by React Error Boundaries
+        // https://reactjs.org/docs/error-boundaries.html
+      )
+      .then((json) =>
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+
+        dispatch(receiveCategories(json)),
+      );
   };
 }
